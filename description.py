cfdb355803b200059b6a423e6c1c12a83c0dce19
HenriqueCastros <henrique.castros@outlook.com>
2023-11-24 21:57:13 -0300

    description: + adds description module

---
+++ description.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+
+def calculate_area(image):
+    """calculate_area
+        Considers any non-zero pixel as part of object. 0 is considered as background.
+
+    Args:
+        image: the image to calculate.  
+
+    Returns:
+        number: the area of the object
+    """
+    return np.count_nonzero(image)
+
+def calculate_perimeter(image):
+    """calculate_perimeter
+        If the image has multiple objects, it will only return one perimeter
+        TODO: return an array with all perimeters
+    
+    Args:
+        image: the image to be calculated
+
+    Returns:
+        number: the perimeter
+    """
+    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    return cv2.arcLength(contours[0], True)
+
+def calculate_centroid(image):
+    """calculate_centroid
+    
+    Args:
+        image: the image to be calculated
+
+    Returns:
+        tuple: the centroid
+    """
+    M = cv2.moments(image)
+    
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+
+    return (cX, cY)
+
+def calculate_compactness(image):
+    """calculate_compactness
+
+    Args:
+        image: the image to be calculated
+
+    Returns:
+        number: compactness
+    """
+    return (calculate_perimeter(image) ** 2) / (4 * np.pi * calculate_area(image))
