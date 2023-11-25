5b7eb9388a564e2bc706c52e66d47081144a8021
HenriqueCastros <henrique.castros@outlook.com>
2023-11-25 12:27:29 -0300

    utils: + translate, crop image and origin

--- src/utils.py
+++ src/utils.py
@@ -18,3 +18,53 @@ def scale_proportional(tuple_to_scale, max_dimensions=(400, 200)):
     scaled_tuple = (int(x * scale_factor), int(y * scale_factor))
 
     return scaled_tuple
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+def crop_image_around_point(image, x, y, crop_size):
+    """crop_image_around_point
+        Crops the image using (x,y) as center. Which means the position of cropped
+    area is ((x - (crop_size / 2), (y - (crop_size / 2)) to 
+    ((x + (crop_size / 2), (y + (crop_size / 2))
+    
+    Args:
+        image (list[list]): image to be cropped
+        x (int): x position
+        y (int): y position
+        crop_size (int): distance from center to be cropped
+
+    Returns:
+        list[list]: cropped image
+    """
+    x_start = max(0, x - int(crop_size / 2))
+    y_start = max(0, y - int(crop_size / 2))
+    x_end = min(image.shape[1], x + int(crop_size / 2))
+    y_end = min(image.shape[0], y + int(crop_size / 2))
+
+    cropped_image = image[y_start:y_end, x_start:x_end]
+
+    return cropped_image
+
+def getOriginFromCropped(x, y, crop_size):
+    """getOriginFromCropped
+
+    Args:
+        x (int): x position
+        y (int): y position
+        crop_size (int): distance from center to be cropped
+
+    Returns:
+        tuple: origin point
+    """
+    x_start = max(0, x - int(crop_size / 2))
+    y_start = max(0, y - int(crop_size / 2))
+    return (x_start, y_start)
